59c6eafdfd1834621f47e281fb15259d93c83734
TFC <3100228060@qq.com>
2021-04-14 22:42:33 +0800

    提交

---
+++ 01_TypeScript/01_第一次的ts.js
@@ -0,0 +1,7 @@
+(function () {
+    function sayHi(str) {
+        return '你好啊' + str;
+    }
+    var text = 'hellow world';
+    console.log(sayHi(text));
+})();
---
+++ 01_TypeScript/01_第一次的ts.ts
@@ -0,0 +1,10 @@
+(()=>{
+    function sayHi(str){
+        return '你好啊' + str
+    }
+    let text = 'hellow world'
+    console.log(sayHi(text))
+})()
+
+
+//tsc .\01_第一次的ts.ts  编译成js文件。
